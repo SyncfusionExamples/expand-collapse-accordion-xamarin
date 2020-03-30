368e611291685763909e8127b6fadcff6ba8ac3a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-30 16:37:47 +0530

    XAMARIN-34012 - Sample added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Model/ItemInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AccordionXamarin
+{
+    public class ItemInfo : INotifyPropertyChanged
+    {
+        private bool isExpand;
+        private string name;
+        private string description;
+
+        public bool IsExpand
+        {
+            get { return isExpand; }
+            set
+            {
+                isExpand = value;
+                OnPropertyChanged("IsExpand");
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
